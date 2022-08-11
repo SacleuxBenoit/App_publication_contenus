a06fb3d24f3a714265fdbfef05a926b4af4decfe
alex62320 <alex.62320@live.fr>
2022-08-11 09:35:42 +0200

    ajout des nuages de mots
    nuage de mots en js,css et html

---
+++ script/app.js
@@ -0,0 +1,60 @@
+// fetch categories for cloud_word
+
+fetch("https://127.0.0.1:8000/api/categories", {
+    method: "GET"
+})
+    .then(response => response.json())
+    .then(function (categories){
+        var categoryList = categories["hydra:member"];
+        categoryList.forEach(category => {
+            // console.log(category.articles);
+            var articleAssigned = category.articles.length;
+            if (articleAssigned == 1) {
+                console.log(articleAssigned)
+                var rand = 1;
+            } else if (articleAssigned == 2){
+                var rand = 2;
+            } else if (articleAssigned == 3){
+                var rand = 3;
+            }else if (articleAssigned >= 4){
+                var rand = 5;
+            } else{
+                var rand = 0;
+            }
+            var addCategory = document.createElement("li");
+            addCategory.innerHTML = `<a href="${category.name}" data-weight="${rand}" >${category.name}</a>`;
+            document.querySelector("#listeCategories").appendChild(addCategory);
+        })
+    });
+
+// fetch tags for cloud_word
+
+fetch("https://127.0.0.1:8000/api/tags", {
+    method: "GET"
+})
+    .then(response => response.json())
+    .then(function (tags){
+        var tagsList = tags["hydra:member"];
+        tagsList.forEach(tag => {
+            var tagAssigned = tag.articles.length;
+            console.log(tagAssigned)
+            console.log(tagAssigned);
+            if (tagAssigned == 1) {
+                console.log(tagAssigned)
+                var rand = 1;
+            } else if (tagAssigned == 2){
+                var rand = 2;
+            } else if (tagAssigned >= 4){
+                var rand = 3;
+            }else if (tagAssigned == 6){
+                var rand = 4;
+            }else if (tagAssigned >= 8){
+                var rand = 5;
+            } else{
+                var rand = 0;
+            }
+            var addTag = document.createElement("li");
+            addTag.innerHTML = `<a href="${tag.name}" data-weight="${rand}" >${tag.name}</a>` ;
+            document.querySelector("#listeTag").appendChild(addTag);
+        })
+    });
