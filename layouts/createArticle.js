a544863a71c0a97d48c59ec892c36be5ad9d6cfb
Quentin Ramecourt <pro.quentin.ramecourt@gmail.com>
2022-08-12 17:52:12 +0200

    modification de l'HTML en fonction des suggestions de l'équipe

---
+++ layouts/createArticle.js
@@ -0,0 +1,49 @@
+//liaison du JS avec les éléments HTML
+const getTitle = document.getElementById('formTitle')
+const getBody = document.getElementById('formBody')
+const getCategory = document.getElementById('formCategory')
+const getWriter= document.getElementById('formWriter')
+const getDate = document.getElementById('formDate')
+const getTags = document.getElementById('formTags')
+const buttonSubmit = document.querySelector('#submit')
+const buttonLocalSubmit = document.querySelector('#submitLocal')
+//URLs from the API used 
+const fetchURL = 'https://127.0.0.1:8000/api/articles';
+const fetchTags = 'https://127.0.0.1:8000/api/tags/';
+const fetchCategories ='https://127.0.0.1:8000/api/categories';
+
+//get data from API to fill dropdown
+function Categories() { 
+//fetch URL categories
+  fetch(fetchCategories, {
+    method: 'GET', 
+    headers: {
+        'Content-Type': 'application/json'
+        }
+    })  .then(function (response) { console.log(response) });
+};
+
+
+
+//send data in localStorage
+function sendToLocal(){
+    /*sends data in localStorage(unlike sessionStorage, localStorage is persistent)*/
+    localStorage.setItem("titre", getTitle.value)
+    let titleItem = localStorage.getItem("titre")
+
+    localStorage.setItem("Body", getBody.value)
+    let bodyItem = localStorage.getItem("body")
+    
+    localStorage.setItem("writer", getWriter.value)
+    let writerItem = localStorage.getItem("writer")
+
+    localStorage.setItem("date", getDate.value)
+    let dateItem = localStorage.getItem("date")
+    //faire de la gestion d'erreurs
+}
+
+
+//eventlisteners
+// buttonSubmit.addEventListener('click', send)
+buttonLocalSubmit.addEventListener('click', sendToLocal)
+
