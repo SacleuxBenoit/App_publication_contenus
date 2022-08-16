8ea5e1e2686e01d5c81d846805cb1341be7ea2b8
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-08-16 11:30:21 +0200

    create folder back-office for css && js + rename article.html to createArticle.html

---
+++ script/back-office/article.js
@@ -0,0 +1,6 @@
+const titleArticle = document.getElementById("titleArticle");
+const bodyArticle = document.getElementById("bodyArticle");
+const tagArticle = document.getElementById("tagArticle");
+const categorieArticle = document.getElementById("categorieArticle");
+const AuteurArticle = document.getElementById("AuteurArticle");
+const dateArticle = document.getElementById("dateArticle");
