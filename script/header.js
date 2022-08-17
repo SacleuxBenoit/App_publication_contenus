28ac31dd485b951a392cab55ce2dbd52e02207d4
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-08-17 14:04:53 +0200

    display tags on dropdown in the header

--- script/header.js
+++ script/header.js
@@ -1,4 +1,6 @@
-const selectArticle = document.querySelector("#select");
+// display categories on a dropdown
+const selectCategories = document.getElementById("selectCategories");
+
 fetch("https://127.0.0.1:8000/api/categories", { method: "GET" })
 .then(function (response) {
   return response.json();
@@ -8,7 +10,24 @@ fetch("https://127.0.0.1:8000/api/categories", { method: "GET" })
     let categoryOption = document.createElement("option");
     categoryOption.innerHTML = category["name"];
     categoryOption.value = category["id"];
-    categoryOption.id = "option-" + category["id"];
-    selectArticle.appendChild(categoryOption);
+    categoryOption.id = "option" + category["id"];
+    selectCategories.appendChild(categoryOption);
+  });
+});
+
+// display tags on a dropdown
+const selectTags = document.getElementById("selectTags");
+
+fetch("https://127.0.0.1:8000/api/tags", { method: "GET" })
+.then(function (response) {
+  return response.json();
+})
+.then((responseJSON) => {
+  responseJSON["hydra:member"].forEach((tag) => {
+    let tagOption = document.createElement("option");
+    tagOption.innerHTML = tag["name"];
+    tagOption.value = tag["id"];
+    tagOption.id = "option" + tag["id"];
+    selectTags.appendChild(tagOption);
   });
 });
